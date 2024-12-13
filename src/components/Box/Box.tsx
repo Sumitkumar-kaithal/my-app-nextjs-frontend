8e1aa1dd55b9e692783a8577208deb35726fa8ac
Sumit Kumar Kaithal <sumitkumarkaithal5@gmail.com>
2024-12-13 18:11:46 +0530

    Update Box.tsx

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,7 +1,7 @@
 const Box= ()=>{
     return(
         <div className=" w-52 h-52  bg-red-500 flex items-center justify-center">
-            <h1>Box</h1>
+            <h1>Okay</h1>
         </div>
     )
 }
